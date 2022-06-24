b1469a39f2d75d149ea3e4f69b8bd1eda1bfdd9e
Kisalay <kisalaykisu@gmail.com>
2022-06-24 19:01:41 +0530

    Welcome File has been added

---
+++ src/com/brigdelabz/Address_Book.java
@@ -0,0 +1,9 @@
+// Welcome Message
+
+package com.brigdelabz;
+
+public class Address_Book{
+    public static void main(String[] args){
+        System.out.println("Welcome to the Address book program");
+    }
+}
